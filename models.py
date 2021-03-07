7ab5e01e57a5f4ab9ccf343c0ef07b8861673f94
Robert Schmidt <rschmidt347@gmail.com>
2021-03-06 21:53:10 -0800

    More checking print statements

--- models.py
+++ models.py
@@ -119,21 +119,19 @@ class BiDAF(nn.Module):
 
         c_emb = self.emb(cw_idxs)  # (batch_size, c_len, hidden_size)
         q_emb = self.emb(qw_idxs)  # (batch_size, q_len, hidden_size)
-        print("c_emb original type:", c_emb.dtype)
 
         if self.use_char_embeddings:
             cc_emb = self.char_emb(cc_idxs)  # (batch_size, c_len, hidden_size)
             qc_emb = self.char_emb(qc_idxs)  # (batch_size, q_len, hidden_size)
             c_emb = torch.cat([c_emb, cc_emb], dim=2)  # (batch_size, c_len, final_context_hidden_size = 2*hidden_size)
             q_emb = torch.cat([q_emb, qc_emb], dim=2)  # (batch_size, q_len, final_hidden_size = 2 * hidden_size)
-            print("c_emb type after char embeds added:", c_emb.dtype)
 
         if self.use_token:
             ner_emb = self.enc_ner(ner_idxs)    # (batch_size, c_len, {token_embed_size OR NUM_NER_TAGS})
             pos_emb = self.enc_pos(pos_idxs)    # (batch_size, c_len, {token_embed_size OR NUM_POS_TAGS})
-            # print("c_emb has shape:", c_emb.shape)
-            # print("ner_emb has shape:", ner_emb.shape)
-            # print("pos_emb has shape:", pos_emb.shape)
+            print("c_emb has shape:", c_emb.shape)
+            print("ner_emb has shape:", ner_emb.shape)
+            print("pos_emb has shape:", pos_emb.shape)
             c_emb = torch.cat([c_emb, ner_emb, pos_emb], dim=2)
             # -> (batch_size, c_len, final_context_hidden_size += {2 * token_embed_size OR (NUM_NER_TAGS+NUM_POS_TAGS)})
 
@@ -142,30 +140,29 @@ class BiDAF(nn.Module):
             exact_orig = torch.unsqueeze(exact_orig, dim=2).float()
             exact_uncased = torch.unsqueeze(exact_uncased, dim=2).float()
             exact_lemma = torch.unsqueeze(exact_lemma, dim=2).float()
-            # print("c_emb has shape:", c_emb.shape)
-            # print("exact_orig has shape:", exact_orig.shape)
-            # print("exact_uncased has shape:", exact_uncased.shape)
-            # print("exact_lemma has shape:", exact_lemma.shape)
+            print("c_emb has shape:", c_emb.shape)
+            print("exact_orig has shape:", exact_orig.shape)
+            print("exact_uncased has shape:", exact_uncased.shape)
+            print("exact_lemma has shape:", exact_lemma.shape)
             # -> (batch_size, c_len, 1)
             c_emb = torch.cat([c_emb, exact_orig, exact_uncased, exact_lemma], dim=2)
             # -> (batch_size, c_len, final_context_hidden_size += 3)
 
-        # print("c_emb shape after features added:", c_emb.shape)
-        print("c_emb type after features added:", c_emb.dtype)
+        print("c_emb shape after features added:", c_emb.shape)
 
         # Project context word embeddings from final_context_hidden_size -> final_hidden_size
         if self.use_exact or self.use_token:
             c_emb = self.project(c_emb)  # (batch_size, c_len, final_hidden_size)
 
-        # print("c_emb shape after projection layer:", c_emb.shape)
-        print("c_emb type:", c_emb.dtype)
+        print("c_emb shape after projection layer:", c_emb.shape)
 
         c_emb = self.hwy(c_emb)  # (batch_size, c_len, final_hidden_size)
         q_emb = self.hwy(q_emb)  # (batch_size, q_len, final_hidden_size)
 
-        # print("c_emb shape after hwy layer:", c_emb.shape)
-        # print("c_len shape:", c_len.shape)
-        print("c_emb type:", c_emb.dtype)
+        print("c_emb shape after hwy layer:", c_emb.shape)
+        print("c_len shape:", c_len.shape)
+        print("q_emb shape:", q_emb.shape)
+        print("q_len shape:", q_len.shape)
 
         # Adjust final_context_hidden_size -> final_hidden_size in enc layer
         c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * final_hidden_size)
