48a3db7bbcf5b0a25ed7415544e9e75ed8f9e86e
Robert Schmidt <rschmidt347@gmail.com>
2021-03-13 12:04:13 -0800

    Add shape print statments

--- models.py
+++ models.py
@@ -130,7 +130,10 @@ class BiDAF(nn.Module):
         q_mask = torch.zeros_like(qw_idxs) != qw_idxs
         c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
 
-        print("Token size: " + str(ner_idxs.shape))
+        print("C-NER size: " + str(ner_idxs.shape))
+        print("C-POS size: " + str(pos_idxs.shape))
+        print("Q-NER size: " + str(qner_idxs.shape))
+        print("Q-POS size: " + str(qpos_idxs.shape))
 
         c_emb = self.emb(cw_idxs)  # (batch_size, c_len, hidden_size)
         q_emb = self.emb(qw_idxs)  # (batch_size, q_len, hidden_size)
