5eb0e01e7dfeb2f783adecb30cc7fbf69f744de9
Robert Schmidt <rschmidt347@gmail.com>
2021-03-13 11:56:54 -0800

    Add print statement

--- models.py
+++ models.py
@@ -130,6 +130,8 @@ class BiDAF(nn.Module):
         q_mask = torch.zeros_like(qw_idxs) != qw_idxs
         c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
 
+        print("Token size: " + str(ner_idxs.size))
+
         c_emb = self.emb(cw_idxs)  # (batch_size, c_len, hidden_size)
         q_emb = self.emb(qw_idxs)  # (batch_size, q_len, hidden_size)
 
