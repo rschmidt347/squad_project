cf9983ebfcd7cdb84e2a02524fa1666d2948c25d
Brian Powell <brian.mac.powell@gmail.com>
2021-03-14 23:12:53 -0400

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -12,16 +12,17 @@ parser.add_argument('--split',
                     default='dev',
                     choices=('dev', 'test'),
                     help='Split to use for ensembling.')
-parser.add_argument('--source_folder',
-                    type=str,
-                    default='',
-                    help='Name for submission file.')
 parser.add_argument('--sub_file',
                     type=str,
                     default='val_submission.csv',
                     help='Name for submission file.')
 
 args = parser.parse_args()
+
+source_folder = './save/' + f'{args.split}' + '_submissions/'
+stats_file = f'{args.split}' + '_stats.csv'
+
+
 
 
 
