2a1ff49f6502d12f117d15b97690df5a94f21594
Brian Powell <brian.mac.powell@gmail.com>
2021-03-14 23:06:48 -0400

    Add files for ensembling

---
+++ ensemble.py
@@ -0,0 +1,27 @@
+"""
+Generate ensemble submission by majority vote.
+"""
+
+import argparse
+
+
+parser = argparse.ArgumentParser('Get args for ensemble script')
+
+parser.add_argument('--split',
+                    type=str,
+                    default='dev',
+                    choices=('dev', 'test'),
+                    help='Split to use for ensembling.')
+parser.add_argument('--source_folder',
+                    type=str,
+                    default='',
+                    help='Name for submission file.')
+parser.add_argument('--sub_file',
+                    type=str,
+                    default='val_submission.csv',
+                    help='Name for submission file.')
+
+args = parser.parse_args()
+
+
+
